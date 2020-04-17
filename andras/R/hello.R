492d5454d2bc7d6a15d5ad40089417c859d90f7a
andras-somkuti <andras@somkuti.net>
2020-04-17 10:02:03 +0000

    Fixed data.table import

--- andras/R/hello.R
+++ andras/R/hello.R
@@ -2,13 +2,15 @@
 #' @export
 #' @param retried the number of retries previously done before the exponential backoff sleep
 #' @importFrom binancer binance_coins_prices
-get_bitcoin_price <- function() {
+#' @import data.table
+get_bitcoin_price <- function(retried = 0) {
   tryCatch(
     binance_coins_prices()[symbol == "BTC", usd],
 
     error = function(e) {
-      sys.sleep(1 + retried^2)
-      get_bitcoin_price(retried = retried + 1)
+      Sys.sleep(1 + retried^2)
+      # get_bitcoin_price(retried = retried + 1)
+      return(e)
     })
 }
 
